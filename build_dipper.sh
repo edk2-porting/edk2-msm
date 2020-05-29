b0fb513726fc2ec5c334ce168608967df49b96f0
strongtz <strongtz@yeah.net>
2020-05-29 16:41:26 +0800

    Sometimes the build script seems to be case sensitive

--- build_dipper.sh
+++ build_dipper.sh
@@ -7,7 +7,7 @@ set -e
 . firstrun_common.sh
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
 GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p sdm845Pkg/dipper.dsc
-gzip -c < workspace/Build/sdm845Pkg/DEBUG_GCC5/FV/sdm845PKG_UEFI.fd >uefi_img
+gzip -c < workspace/Build/sdm845Pkg/DEBUG_GCC5/FV/SDM845PKG_UEFI.fd >uefi_img
 cat ./device_specific/dipper.dtb >>uefi_img
 abootimg --create boot_dipper.img -k uefi_img -r ramdisk -f bootimg.cfg
 rm -rf ./uefi_img
