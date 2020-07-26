0dc6f5bb62ce035422c6289632d040924b7ff8a7
acai66 <1779864536@qq.com>
2020-07-27 01:34:39 +0800

    Add support for Oneplus 6

---
+++ build_enchilada.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# based on the instructions from edk2-platform
+echo cleanning BuidFiles
+rm -rf boot_enchilada.img
+echo Done.
+set -e
+. firstrun_common.sh
+# not actually GCC5; it's GCC7 on Ubuntu 18.04.
+GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p sdm845Pkg/enchilada.dsc
+gzip -c < workspace/Build/sdm845Pkg/DEBUG_GCC5/FV/SDM845PKG_UEFI.fd >uefi_img
+cat ./device_specific/enchilada.dtb >>uefi_img
+abootimg --create boot_enchilada.img -k uefi_img -r ramdisk -f bootimg.cfg
+rm -rf ./uefi_img
