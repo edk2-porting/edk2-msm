1b9f37c704b36b6c3dcb2ade4de692db9b203c9b
BigfootACA <bigfoot@classfun.cn>
2020-10-08 00:09:09 +0800

    merge all scripts to build.sh

--- firstrun.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# based on the instructions from edk2-platform
-# do this first:
-# https://github.com/tianocore/tianocore.github.io/wiki/Using-EDK-II-with-Native-GCC#Install_required_software_from_apt
-set -e
-. firstrun_common.sh
-make -C ../edk2/BaseTools
-
