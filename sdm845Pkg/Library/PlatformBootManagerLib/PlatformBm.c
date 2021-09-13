d1d6fc77f6b1b7768d44696db30f5c7537f685df
BigfootACA <bigfoot@classfun.cn>
2021-09-13 15:37:29 +0800

    sdm845Pkg: Library: PlatformBootManagerLib: PlatformBm.c: only register option when simple-init enabled
    Signed-off-by: BigfootACA <bigfoot@classfun.cn>

--- sdm845Pkg/Library/PlatformBootManagerLib/PlatformBm.c
+++ sdm845Pkg/Library/PlatformBootManagerLib/PlatformBm.c
@@ -741,12 +741,14 @@ PlatformBootManagerAfterConsole (
   //
   EfiBootManagerRefreshAllBootOption ();
 
+  #ifdef ENABLE_SIMPLE_INIT
   //
   // Register Simple Init GUI APP
   //
   PlatformRegisterFvBootOption (
   &gSimpleInitFileGuid, L"Simple Init", LOAD_OPTION_ACTIVE
   );
+  #endif
 
   //
   // Register UEFI Shell
