df9b776e2eed0cdd5bc9bc41dd495b7b0b719a7e
Xilin Wu <strongtz@yeah.net>
2022-10-20 22:21:18 +0800

    Add SetCPUFreqDxe to sdm845 and sm7325
    This dxe will set cpu speed to maximum right after ClockDxe loads.
    It should help to speed up boot process.

---
+++ Platform/RenegadePkg/Drivers/SetCPUFreqDxe/SetCPUFreqDxe.c
@@ -0,0 +1,58 @@
+#include <Library/PcdLib.h>
+#include <Library/DebugLib.h>
+#include <Library/UefiDriverEntryPoint.h>
+#include <Library/UefiBootServicesTableLib.h>
+
+#include <Protocol/EFIClock.h>
+
+STATIC EFI_HANDLE Handle = NULL;
+
+EFI_STATUS
+EFIAPI
+SetCPUFreqDxeMain (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+  )
+{
+  EFI_STATUS             Status                       = EFI_SUCCESS;
+  EFI_CLOCK_PROTOCOL    *pClockProtocol               = NULL;
+  UINT32                 perfLevel                    = 0;
+  UINT32                 frequencyHz                  = 0;
+
+  Status = gBS->LocateProtocol(
+      &gEfiClockProtocolGuid, 
+      NULL, 
+      (VOID **)&pClockProtocol
+  );
+
+  if (EFI_ERROR(Status)) {
+    DEBUG((EFI_D_INFO, "%a: Failed to locate protocol\n", __FUNCTION__));
+    return Status;
+  }
+
+  DEBUG((EFI_D_INFO, "\n\n\n\n\n\n\n\n\n\n\n\n\n"));
+
+  // one A55 cluster, two A78 clusters, one L3 cache on sm7325
+  // here we assume 4 cpu + 4 cpu + L3 cache
+  for (int i = 0; i < 9; i++) {
+    Status = pClockProtocol->GetMaxPerfLevel(pClockProtocol, i, &perfLevel);
+
+    if (EFI_ERROR(Status)) {
+      DEBUG((EFI_D_INFO, "%a: Failed to get the maximum performance level for CPU %d, Status: %r\n", __FUNCTION__, i, Status));
+      DEBUG((EFI_D_INFO, "%a: This CPU may not exist on current platform\n", __FUNCTION__));
+      return Status;
+    }
+
+    Status = pClockProtocol->SetCpuPerfLevel(pClockProtocol, i, perfLevel, &frequencyHz);
+
+    if (EFI_ERROR(Status)) {
+      DEBUG((EFI_D_INFO, "%a: Failed to set the maximum performance level for CPU %d, Status: %r\n", __FUNCTION__, i, Status));
+      return Status;
+    }
+
+    DEBUG((EFI_D_INFO, "%a: CPU %d Now running at %d Hz\n", __FUNCTION__, i, frequencyHz));
+  }
+
+  // gBS->Stall(10 * 1000 * 1000);
+  return Status;
+}
