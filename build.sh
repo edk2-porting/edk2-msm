f3f8f54dfe316c814af2b589942cdac407185155
sunhuish <sunshuih@hotmail.com>
2023-03-20 17:24:22 +0800

    Update build.sh chinese submodule

--- build.sh
+++ build.sh
@@ -214,34 +214,37 @@ then
 	echo "Updating submodules"
 	if "${CHINESE}"
 	then
-		git submodule set-url Common/edk2                                           https://hub.nuaa.cf/tianocore/edk2.git
-		git submodule set-url Common/edk2-platforms                                 https://hub.nuaa.cf/tianocore/edk2-platforms.git
-		git submodule set-url Platform/EFI_Binaries                                 https://hub.nuaa.cf/edk2-porting/edk2-sdm845-binary.git
-		git submodule set-url GPLDrivers/Library/SimpleInit                         https://hub.nuaa.cf/BigfootACA/simple-init.git
-		git submodule set-url tools/Installer                                       https://hub.nuaa.cf/MollySophia/Flashable_UEFI_Installer.git
+		vim -u NONE -N .gitmodules -c "%s/github.com/hub.nuaa.cf/g" -c ":wq"
 		git submodule init;git submodule update --depth 1
 		pushd Common/edk2
-
-		git submodule set-url ArmPkg/Library/ArmSoftFloatLib/berkeley-softfloat-3   https://hub.nuaa.cf/ucb-bar/berkeley-softfloat-3.git
-		git submodule set-url CryptoPkg/Library/OpensslLib/openssl                  https://hub.nuaa.cf/openssl/openssl.git
-		git submodule set-url BaseTools/Source/C/BrotliCompress/brotli              https://hub.nuaa.cf/google/brotli.git
-		git submodule set-url UnitTestFrameworkPkg/Library/CmockaLib/cmocka         https://hub.nuaa.cf/tianocore/edk2-cmocka.git
-		git submodule set-url ArmPkg/Library/ArmSoftFloatLib/berkeley-softfloat-3   https://hub.nuaa.cf/ucb-bar/berkeley-softfloat-3.git
-		git submodule set-url MdeModulePkg/Library/BrotliCustomDecompressLib/brotli https://hub.nuaa.cf/google/brotli.git
-		git submodule set-url MdeModulePkg/Universal/RegularExpressionDxe/oniguruma https://hub.nuaa.cf/kkos/oniguruma.git
-		git submodule set-url RedfishPkg/Library/JsonLib/jansson                    https://hub.nuaa.cf/akheron/jansson.git
+		vim -u NONE -N .gitmodules -c "%s/github.com/hub.nuaa.cf/g" -c ":wq"
+		git submodule init;git submodule update
+		pushd CryptoPkg/Library/OpensslLib/openssl
+		git submodule set-url boringssl https://hub.nuaa.cf/google/boringssl
+		vim -u NONE -N .gitmodules -c "%s/github.com/hub.nuaa.cf/g" -c ":wq"
+		git submodule init;git submodule update
+		git checkout .gitmodules
+		popd
+		git checkout .gitmodules
+		popd
+		pushd Common/edk2-platforms
+		vim -u NONE -N .gitmodules -c "%s/github.com/hub.nuaa.cf/g" -c ":wq"
 		git submodule init;git submodule update
 		git checkout .gitmodules
 		popd
 		pushd GPLDrivers/Library/SimpleInit
-		git submodule set-url libs/lvgl     https://hub.nuaa.cf/lvgl/lvgl.git
 		git submodule set-url libs/freetype https://hub.nuaa.cf/freetype/freetype.git
+		vim -u NONE -N .gitmodules -c "%s/github.com/hub.nuaa.cf/g" -c ":wq"
 		git submodule init;git submodule update
 		popd
 		git checkout .gitmodules
+		git submodule init;git submodule update --recursive
 	else
 		git submodule init;git submodule update --depth 1
 		pushd Common/edk2
+		git submodule init;git submodule update
+		popd
+		pushd Common/edk2-platforms
 		git submodule init;git submodule update
 		popd
 		pushd GPLDrivers/Library/SimpleInit
