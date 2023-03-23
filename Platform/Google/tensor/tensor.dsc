d78a937b33eb86de09db2026bf49a662d5555c82
Marijan Limov <marijan.limov@gmail.com>
2023-03-23 20:53:18 +0100

    raven: Fix clocks

--- Platform/Google/tensor/tensor.dsc
+++ Platform/Google/tensor/tensor.dsc
@@ -23,23 +23,26 @@
 !include Silicon/Google/GooglePkg/GoogleCommonDsc.inc
 
 [PcdsFixedAtBuild.common]
-  gArmTokenSpaceGuid.PcdSystemMemoryBase|0x880000000         # Starting address
+  gArmTokenSpaceGuid.PcdSystemMemoryBase|0x8AF000000         # Starting address
   gArmTokenSpaceGuid.PcdSystemMemorySize|0x0FDFA0000         # Limit to 4GB Size here
 
-  gArmTokenSpaceGuid.PcdCpuVectorBaseAddress|0x880000000     # CPU Vectors
-  gArmTokenSpaceGuid.PcdArmArchTimerFreqInHz|0x1770000
-  gArmTokenSpaceGuid.PcdArmArchTimerSecIntrNum|13
-  gArmTokenSpaceGuid.PcdArmArchTimerIntrNum|14
+  gArmTokenSpaceGuid.PcdCpuVectorBaseAddress|0xfd902000     # CPU Vectors
+  gArmTokenSpaceGuid.PcdArmArchTimerFreqInHz|24576000
+  gArmTokenSpaceGuid.PcdArmArchTimerSecIntrNum|19
+  gArmTokenSpaceGuid.PcdArmArchTimerIntrNum|20
+  gArmTokenSpaceGuid.PcdArmArchTimerVirtIntrNum|27
+  gArmTokenSpaceGuid.PcdArmArchTimerHypIntrNum|26
   gArmTokenSpaceGuid.PcdGicDistributorBase|0x10400000
   gArmTokenSpaceGuid.PcdGicRedistributorsBase|0x10440000
+  gArmTokenSpaceGuid.PcdGicInterruptInterfaceBase|25
 
   gEfiMdeModulePkgTokenSpaceGuid.PcdAcpiDefaultOemRevision|0x00000850
-  gEmbeddedTokenSpaceGuid.PcdPrePiStackBase|0xfd800000      # UEFI Stack
+  gEmbeddedTokenSpaceGuid.PcdPrePiStackBase|0x90600000      # UEFI Stack
   gEmbeddedTokenSpaceGuid.PcdPrePiStackSize|0x00040000      # 256K stack
   #gEmbeddedTokenSpaceGuid.PcdPrePiCpuIoSize|44
 
-  gQcomTokenSpaceGuid.PcdUefiMemPoolBase|0x880010000         # DXE Heap base address
-  gQcomTokenSpaceGuid.PcdUefiMemPoolSize|0x2E000000         # UefiMemorySize, DXE heap size
+  gQcomTokenSpaceGuid.PcdUefiMemPoolBase|0x97400000         # DXE Heap base address
+  gQcomTokenSpaceGuid.PcdUefiMemPoolSize|0x1fe00000         # UefiMemorySize, DXE heap size
   
   gQcomTokenSpaceGuid.PcdMipiFrameBufferAddress|0xfd90c000
 
--- Silicon/Google/Tensor/Library/PlatformMemoryMapLib/PlatformMemoryMapLib.c
+++ Silicon/Google/Tensor/Library/PlatformMemoryMapLib/PlatformMemoryMapLib.c
@@ -5,17 +5,18 @@ static ARM_MEMORY_REGION_DESCRIPTOR_EX gDeviceMemoryDescriptorEx[] = {
     /* Name               Address     Length      HobOption        ResourceAttribute    ArmAttributes
                                                           ResourceType          MemoryType */
     
-    {"PStore",        0xfd3ff000, 0x00200000, AddMem, SYS_MEM, SYS_MEM_CAP, Reserv, WRITE_BACK_XN},
+    {"PStore",        0xfd3ff000, 0x00100000, AddMem, SYS_MEM, SYS_MEM_CAP, Reserv, WRITE_THROUGH_XN},
+    {"NotPStore",        0xFD4FF000, 0x00100000, AddMem, SYS_MEM, SYS_MEM_CAP, Reserv, WRITE_THROUGH_XN},
 
     
     {"UEFI FD",           0xfa800000, 0x02000000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK},
-    {"CPU Vectors",       0x880000000, 0x00001000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK},
-    {"UEFI Stack",        0xfd800000, 0x00040000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK_XN},
-    {"DXE Heap",          0x880010000, 0x2E000000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK_XN},
+    {"CPU Vectors",       0xfd902000, 0x00001000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK},
+    {"UEFI Stack",        0x90600000, 0x00040000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK},
+    {"DXE Heap",          0x97400000, 0x1fe00000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv, WRITE_BACK_XN},
     
     {"Display Reserved",  0xfd90c000, 0x1123000, AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_THROUGH_XN},//incorrect framebuffer address
     
-    {"RAM Partition",     0x8AF000000, 0x0FDFA0000, AddMem,  SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK_XN},
+    {"RAM Partition",     0x8AF000000, 0x0FDFA0000, Mem4G,  SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK},
     
     {"APSS_GIC500_GICD",  0x10400000, 0x00010000,  AddDev, MMAP_IO, UNCACHEABLE, MmIO,   NS_DEVICE},
     {"APSS_GIC500_GICR",  0x10440000, 0x00100000,  AddDev, MMAP_IO, UNCACHEABLE, MmIO,   NS_DEVICE},
